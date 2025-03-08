9735a90a54b2fba7c9f87b01c979e6af30aaff9c
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-08 13:45:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-docement.addEventListener('DOMContentLoaded',()=>{
-	document.body.innerHTML="DOM load success"
-})
+document.addEventListener('DOMContentLoaded', function() {
+  document.body.innerHTML = 'DOM load success';
+});
